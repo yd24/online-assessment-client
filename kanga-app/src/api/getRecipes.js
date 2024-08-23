424c5ba208ca6e5e21e4d9c53c6e6189fbd68f1d
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-08-23 15:50:10 -0700

    Update API links

--- kanga-app/src/api/getRecipes.js
+++ kanga-app/src/api/getRecipes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getRecipes() {
     try {
-        const response = await axios.get('http://localhost:8000/restapi/recipe/');
+        const response = await axios.get('https://online-assessment-server.onrender.com/restapi/recipe/');
         return response.data;
     } catch (error) {
         console.error(error);
--- kanga-app/src/api/updateRecipe.js
+++ kanga-app/src/api/updateRecipe.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function updateRecipe(updatedRecipe) {
     try {
         const id = updatedRecipe.id;
-        const response = await axios.put(`http://localhost:8000/restapi/recipe/${id}/`, updatedRecipe);
+        const response = await axios.put(`https://online-assessment-server.onrender.com/restapi/recipe/${id}/`, updatedRecipe);
         return response.data;
     } catch (error) {
         console.error(error);
